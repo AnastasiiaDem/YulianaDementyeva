a395074ec076f57720227831656b96910da67715
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-07-07 19:58:13 -0400

    project init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import bookData from './book-data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Yuliana';
+export class AppComponent implements OnInit {
+
+  showName = true;
+  items: any = [];
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let book = (document.querySelector('#book') as HTMLElement).clientHeight;
+    let runway = (document.querySelector('#runway') as HTMLElement).clientHeight;
+    let polaroid = (document.querySelector('#polaroid') as HTMLElement).clientHeight;
+    let name = document.querySelector('.name') as HTMLElement;
+
+    this.showName = window.pageYOffset < runway;
+  }
+
+  ngOnInit(): void {
+    this.items = bookData.data;
+  }
+
 }
