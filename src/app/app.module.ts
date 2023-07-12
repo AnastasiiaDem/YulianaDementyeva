1903f4e8b59ff1bdf7c2e5f4413349ae9cb419eb
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-07-11 22:20:05 -0400

    moved all images on google disk, added some pages and made some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { MdbCarouselModule } from 'mdb-angular-ui-kit/carousel';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DetailsComponent} from './moonaya/details.component';
+import {DetailsComponent} from './details/details.component';
 import {AppRoutingModule} from './app-routing.module';
 import { MainComponent } from './main/main.component';
 import {NgxSpinnerModule} from 'ngx-spinner';
