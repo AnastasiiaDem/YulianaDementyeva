1df34212f0b3b9f0aa009653b91f65d169800ed2
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-07-11 22:15:08 -0400

    moved all images on google disk, added some pages and made some fixes

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,34 @@
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import detailData from '../details-data.json';
+import {Router} from '@angular/router';
+import {NgxSpinnerService} from 'ngx-spinner';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+export class DetailsComponent implements OnInit, AfterViewInit {
+  displayData: any;
+  title: any;
+  grid: any;
+
+  constructor(private router: Router,
+              private spinner: NgxSpinnerService) {
+    this.title = this.router.getCurrentNavigation()?.extras.state?.['title'];
+    this.displayData = detailData.data.find(e => {
+      return e.title == this.title;
+    });
+    this.grid = this.displayData.grid;
+  }
+
+  ngOnInit() {
+    this.spinner.show();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.spinner.hide();
+    }, 2000);
+  }
+}
